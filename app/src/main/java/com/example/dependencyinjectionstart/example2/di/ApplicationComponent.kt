0dfafec65aacb4c38aec4e9612b9a59f10274d4b
SolomonVP <Lum7023@yandex.ru>
2024-10-20 21:38:03 +0300

    use custome factory... 03:23

--- app/src/main/java/com/example/dependencyinjectionstart/example2/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/di/ApplicationComponent.kt
@@ -3,6 +3,7 @@ package com.example.dependencyinjectionstart.example2.di
 import android.content.Context
 import com.example.dependencyinjectionstart.example2.presentation.MainActivity
 import dagger.BindsInstance
+import dagger.BindsOptionalOf
 import dagger.Component
 
 @Component(modules = [DataModule::class, DomainModule::class])
@@ -10,16 +11,14 @@ interface ApplicationComponent {
 
     fun inject(activity: MainActivity)
 
-    @Component.Builder
-    interface ApplicationComponentBuilder {
+    @Component.Factory
+    interface ApplicationComponentFactory {
 
-        @BindsInstance
-        fun context(context: Context): ApplicationComponentBuilder
-
-        @BindsInstance
-        fun timeMillis(timeMillis: Long): ApplicationComponentBuilder
-
-        fun build(): ApplicationComponent
+        fun create(
+            @BindsInstance context: Context,
+            @BindsInstance timeMillis: Long
+        ): ApplicationComponent
 
     }
+
 }
