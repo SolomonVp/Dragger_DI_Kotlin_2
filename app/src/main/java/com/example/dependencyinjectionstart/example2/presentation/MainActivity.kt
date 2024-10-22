ebca7c72f29c9546dca37d31aceae35dc114a4c3
SolomonVP <Lum7023@yandex.ru>
2024-10-22 21:56:42 +0300

    Inject ViewModel - для одной viewModel

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.dependencyinjectionstart.example2.presentation
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.dependencyinjectionstart.R
 import com.example.dependencyinjectionstart.example2.ExampleApp
 import com.example.dependencyinjectionstart.example2.data.database.ExampleDatabase
@@ -12,7 +13,11 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity() {
 
     @Inject
-    lateinit var viewModel: ExampleViewModel
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private val viewModel by lazy {
+        ViewModelProvider(this, viewModelFactory)[ExampleViewModel::class.java]
+    }
 
     private val component by lazy {
         (application as ExampleApp).component
